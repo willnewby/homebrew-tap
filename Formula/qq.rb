e65b2c0ca7b0690964bc2b05e789e030e4d32bc2
goreleaserbot <goreleaser@example.com>
2025-03-11 19:58:27 -0600

    Brew formula update for qq version v0.1.7

--- Formula/qq.rb
+++ Formula/qq.rb
@@ -5,13 +5,13 @@
 class Qq < Formula
   desc "A simple, fast job queue based on River Queue"
   homepage "https://github.com/willnewby/qq"
-  version "0.1.6"
+  version "0.1.7"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/willnewby/qq/releases/download/v0.1.6/qq_Darwin_x86_64.tar.gz"
-      sha256 "016b4c71c854bc71685bb4ce2f103f29765947f90b6eab9bfe7e911920ea7fd2"
+      url "https://github.com/willnewby/qq/releases/download/v0.1.7/qq_Darwin_x86_64.tar.gz"
+      sha256 "1c86195384b0193bb009d5379f0faa44fd2f5ae3862d1e0bb33c965e24275d53"
 
       def install
         bin.install "qq"
@@ -28,8 +28,8 @@ class Qq < Formula
       end
     end
     on_arm do
-      url "https://github.com/willnewby/qq/releases/download/v0.1.6/qq_Darwin_arm64.tar.gz"
-      sha256 "46966e5e8694cfd66bf8466ebc8fb6fd4ecb957d01d0a2205eeef341c7496473"
+      url "https://github.com/willnewby/qq/releases/download/v0.1.7/qq_Darwin_arm64.tar.gz"
+      sha256 "78279591ba8cbbd5b597ab67585d4ba9777a3c48b79147c5c07cfd6730142143"
 
       def install
         bin.install "qq"
@@ -50,8 +50,8 @@ class Qq < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/willnewby/qq/releases/download/v0.1.6/qq_Linux_x86_64.tar.gz"
-        sha256 "49287287cf6e16aec86c873be45373d0de7b4aa6dbcfa3015cf31e7af8b8651a"
+        url "https://github.com/willnewby/qq/releases/download/v0.1.7/qq_Linux_x86_64.tar.gz"
+        sha256 "768b9c3cfc1c08d4149f9ebd6532f8987a9346bedd62f4a8b2b99c31b733a857"
 
         def install
           bin.install "qq"
@@ -70,8 +70,8 @@ class Qq < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/willnewby/qq/releases/download/v0.1.6/qq_Linux_arm64.tar.gz"
-        sha256 "64ed0008035dd7afa5ee07cf8afd2d48ccb92125389b149c393e9a77f50af49e"
+        url "https://github.com/willnewby/qq/releases/download/v0.1.7/qq_Linux_arm64.tar.gz"
+        sha256 "cf10589167e98ea3db84e3192f88730c2cc18d14ad891dc2c99564278231deab"
 
         def install
           bin.install "qq"
